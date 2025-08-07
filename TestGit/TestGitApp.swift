d41ef40c5c51a92452356266df55f7612e6bbf13
GaliSrikanth <GaliSrikanth19@gmail.com>
2025-08-07 19:49:16 +0530

    Initial Commit

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by GaliSrikanth on 07/08/25.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
